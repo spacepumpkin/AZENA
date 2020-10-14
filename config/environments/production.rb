a848889ab03644e0b43e063931182acce3b01335
spacepumpkin <68368260+spacepumpkin@users.noreply.github.com>
2020-10-13 23:42:00 -0700

    Replace all instances of old project name (AsanaClone) with Azena.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "AsanaClone_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "Azena_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
 

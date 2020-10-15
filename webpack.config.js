3538c8527bf7ba03e71b2ec4a1b4c8e0860a259b
spacepumpkin <68368260+spacepumpkin@users.noreply.github.com>
2020-10-14 23:07:39 -0700

    Change Uglifier config in production.rb, swapp webpack and webpack-cli to global dependencies, edit mode in package.json and webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 module.exports = {
+  mode: process.env.NODE_ENV === "production" ? "production" : "development",
   context: __dirname,
   entry: "./frontend/entry.jsx",
   output: {
@@ -18,38 +19,10 @@ module.exports = {
             presets: ["@babel/env", "@babel/react"] // tells babel what syntaxes to translate
           }
         },
-      },
-      {
-        test: /\.(jpe?g|png|gif|svg)$/i,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              query: {
-                name: 'assets/[name].[ext]'
-              }
-            }
-          },
-          {
-            loader: 'image-webpack-loader',
-            options: {
-              query: {
-                mozjpeg: {
-                  progressive: true,
-                },
-                gifsicle: {
-                  interlaced: true,
-                },
-                optipng: {
-                  optimizationLevel: 7,
-                }
-              }
-            }
-          }]
       }
     ]
   },
-  devtool: "source-map",
+  devtool: process.env.NODE_ENV === "production" ? "none" : "source-map",
   resolve: {
     extensions: [".js", ".jsx", ".png", "*"]
   }

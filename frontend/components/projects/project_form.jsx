c17210e64165f591469b909c7e3ae4ffb373a876
spacepumpkin <68368260+spacepumpkin@users.noreply.github.com>
2020-12-19 00:11:49 -0800

    Refactor project form into functional component

--- frontend/components/projects/project_form.jsx
+++ frontend/components/projects/project_form.jsx
@@ -1,48 +1,40 @@
 import React from 'react';
 
-class ProjectForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this._nullState = {
-      name: "",
-      description: ""
-    }
-    this.handleClose = this.handleClose.bind(this);
+const ProjectForm = function ({ workspaceId = -1, history }) {
+  const [name, setName] = React.useState("");
+  const [description, setDescription] = React.useState("");
+
+  const handleClose = () => {
+    history && history.goBack();
   }
 
-  handleClose() {
-    this.props.history && this.props.history.goBack();
-  }
-
-  handleSubmit(evt) {
+  const handleSubmit = (evt) => {
     evt.preventDefault();
-
+    console.log("New Project Created: ", name);
   }
 
-  render() {
-    return (
-      <div id="project-form-modal-wrapper">
-        <div className="modal-backdrop"></div>
-        <div id="project-form-box">
-          <div className="modal-close" onClick={this.handleClose}><span>Close</span></div>
-          <h1>Create a New Project</h1>
-          <form id="project-form" onSubmit={this.handleSubmit}>
-            <label> Project Name
+  return (
+    <div id="project-form-modal">
+      <div className="modal-backdrop"></div>
+      <div id="project-form-box">
+        <div className="modal-close" onClick={handleClose}><span>Close</span></div>
+        <h1>Create a New Project</h1>
+        <form id="project-form" onSubmit={handleSubmit}>
+          <label className={`label-input-focused`} > Project Name
               <input type="text" />
-            </label>
-            <div className="workspace-error"></div>
+          </label>
+          <div className="workspace-error"></div>
 
-            <label> Description
+          <label> Description
               <textarea></textarea>
-            </label>
-            <div className="workspace-error"></div>
-            <button type="button">Submit</button>
-          </form>
-        </div>
-
+          </label>
+          <div className="workspace-error"></div>
+          <button type="button">Submit</button>
+        </form>
       </div>
-    )
-  }
+
+    </div>
+  )
 }
 
 export default ProjectForm;

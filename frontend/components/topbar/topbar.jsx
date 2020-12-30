973618ffc9f83bd4d5329590501a756b11d7f68c
spacepumpkin <68368260+spacepumpkin@users.noreply.github.com>
2020-12-30 14:17:55 -0800

    Add temporary topbar profile links, adjust user avatar padding 1px

--- frontend/components/topbar/topbar.jsx
+++ frontend/components/topbar/topbar.jsx
@@ -237,10 +237,10 @@ export default class TopBar extends React.Component {
         </div>
 
         <div id="topbar-user">
-          {/* <div className="topbar-profile-links">
+          <div className="topbar-profile-links">
             <a href="https://github.com/spacepumpkin/AZENA"><img src={window.github} alt="github profile" /></a>
             <a href="https://www.linkedin.com/in/gary-w-269749ba/"><img src={window.linkedin} alt="linkedin profile" /></a>
-          </div> */}
+          </div>
           {/* User Settings + Global Actions */}
           <div id="user-avatar">
             <button id="user-avatar-button" type="button"
